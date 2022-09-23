52d0b9d19448bb23b6239c2b1f6e8dd337c8cc97
parthlr <parthlr321@gmail.com>
2022-09-23 13:53:42 -0400

    Fixed scenes 6-10

--- Assets/Scripts/BrokenScripts/7/MovementActivator7.cs
+++ Assets/Scripts/BrokenScripts/7/MovementActivator7.cs
@@ -18,12 +18,12 @@ public class MovementActivator7 : MonoBehaviour
         
     }
 
-    private void onTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
         objectToActivate.GetComponent<MoveObject7>().enabled = true;
         otherObjectToActivate.GetComponent<MoveObject7>().enabled = true;
     }
-    private void pnTriggerExit(Collider other)
+    private void OnTriggerExit(Collider other)
     {
         objectToActivate.GetComponent<MoveObject7>().enabled = true;
         otherObjectToActivate.GetComponent<MoveObject7>().enabled = true;
